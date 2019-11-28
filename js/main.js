ace859dd900f1ba85a9bdb176b9872b26a5c1257
blackrose9 <sarah.mwendwa@gmail.com>
2019-11-28 15:16:03 +0300

    saved unsaved changes.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2, number3) {
+    return number1 + number2;
+    }
+    var number1 = parseInt(prompt("Enter a number"))
+    var number2 = parseInt(prompt("Enter another number"))
+    alert(add(number1, number2))
